f394f17afdb52b80fb9cd1f58f3476281869a30f
manilal-1084 <61118893+manilal-1084@users.noreply.github.com>
2024-09-02 00:55:19 +0530

    Update App.js

--- week-1-handson/App.js
+++ week-1-handson/App.js
@@ -1,11 +1,28 @@
 
 
-const heading = React.createElement(
-    "h1",
-    {id: "heading", xyz:"abc"},
-    "Hello World from React!!"
-);
+// const heading = React.createElement(
+//     "h1",
+//     {id: "heading", xyz:"abc"},
+//     "Hello World from React!!"
+// );
 
-console.log(heading);
+// console.log(heading);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+
+// --------------------------------------------------------------------------------------
+
+
+const parent = React.createElement(
+    "div",
+    {id:"parent"},
+    React.createElement(
+        "div",
+        {id:"child"},
+        React.createElement("h1",{},"I'm a h1 tag")
+    )
+);
+console.log(parent); //object
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
